f5037281bd0322294bbdaf6b12ed7bee54ddd834
Elena <buciumeanu.elena@pedagogic.ro>
2025-02-05 10:42:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openArticles() {
+    window.location.href = 'index.html'
+}
+
+function openContact() {
+    window.location.href = 'contact.html'
+}
+
+function openReviews() {
+    window.location.href = 'reviews.html'
+}
